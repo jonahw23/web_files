e385e487721ea1332a4510f94d59ca266213e5ca
jonahw23 <jonahwitte10@gmail.com>
2023-06-08 14:36:16 -0400

    Test mail

--- storedata.php
+++ storedata.php
@@ -1,21 +1,23 @@
-<?php
-  $name = $_POST['Name'];
-  $visitor_email = $_POST['Email'];
-  $message = $_POST['Message'];
+<?php 
 
-    $email_from = 'yourname@yourwebsite.com';
+error_reporting(-1);
+ini_set('display_errors', 'On');
+set_error_handler("var_dump");
 
-    $email_subject = "New Form submission";
+if(isset($_POST['Submit'])){
+    $to = "jonahwitte10@gmail.com"; // this is your Email address
+    $from = $_POST['Email']; // this is the sender's Email address
+    $name = $_POST['Name'];
+    $subject = "Form submission";
+    $subject2 = "Copy of your form submission";
+    $message = $name . " wrote the following:" . "\n\n" . $_POST['Message'];
 
-    $email_body = "You have received a new message from the user $name.\n".
-                        "Here is the message:\n $message".
-
-  $to = "yourname@yourwebsite.com";
-
-  $headers = "From: $email_from \r\n";
-
-  $headers .= "Reply-To: $visitor_email \r\n";
-
-  mail($to,$email_subject,$email_body,$headers);
-
- ?>
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    $checkMail = mail($to,$subject,$message,$headers);
+    echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
+    echo $checkMail;
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    // You cannot use header and echo together. It's one or the other.
+    }
+?>
